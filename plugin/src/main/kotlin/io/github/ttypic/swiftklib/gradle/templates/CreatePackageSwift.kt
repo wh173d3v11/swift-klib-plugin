2286d680ddfdc057e077e9306ad9e11cb54806f2
wh173d3v11 <46423041+wh173d3v11@users.noreply.github.com>
2024-10-04 01:28:38 +0530

    temparory fix - tested with localmaven

--- plugin/src/main/kotlin/io/github/ttypic/swiftklib/gradle/templates/CreatePackageSwift.kt
+++ plugin/src/main/kotlin/io/github/ttypic/swiftklib/gradle/templates/CreatePackageSwift.kt
@@ -8,17 +8,21 @@ internal fun createPackageSwiftContents(
 
     let package = Package(
         name: "$cinteropName",
+        platforms: [.iOS(.v13),
+                    .macOS(.v11)],
         products: [
             .library(
                 name: "$cinteropName",
                 type: .static,
                 targets: ["$cinteropName"])
         ],
-        dependencies: [],
+        dependencies: [
+        .package(name: "onnxruntime", url: "https://github.com/microsoft/onnxruntime-swift-package-manager", .exact("1.19.2")),
+        ],
         targets: [
             .target(
                 name: "$cinteropName",
-                dependencies: [],
+                dependencies: ["onnxruntime"],
                 path: "$cinteropName")
         ]
     )
